7b9abb03411c33bb75d7255db72fc760895d6615
Sebastián Millán <sebasmillan02@gmail.com>
2020-04-18 13:48:39 -0500

    Update

---
+++ Assets/Scripts/Botones.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Botones : MonoBehaviour
+{
+	public void BotonSalir()
+	{
+		Application.Quit();
+	}
+	public void BotonPlay()
+	{
+		SceneManager.LoadScene("Game");
+	}
+}
